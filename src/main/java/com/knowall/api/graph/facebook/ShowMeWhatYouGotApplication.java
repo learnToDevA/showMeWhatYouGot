f119c1dcd1f745f86c26208d5568bb18dbdc2960
Michal Majewski <greenkiwideveloper@gmail.com>
2022-05-30 22:33:25 +0200

    Generate a template for the Spring Boot project

---
+++ src/main/java/com/knowall/api/graph/facebook/ShowMeWhatYouGotApplication.java
@@ -0,0 +1,13 @@
+package com.knowall.api.graph.facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMeWhatYouGotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowMeWhatYouGotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowall/api/graph/facebook/ShowMeWhatYouGotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowall.api.graph.facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMeWhatYouGotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
